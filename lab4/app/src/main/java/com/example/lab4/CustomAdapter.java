e17a5a4d605e05f3c367e1987fc769cde24d8fe3
ibrahimahdy <i.mahdy93@gmail.com>
2019-10-01 15:21:43 +0300

    hw 4 done

--- lab4/app/src/main/java/com/example/lab4/CustomAdapter.java
+++ lab4/app/src/main/java/com/example/lab4/CustomAdapter.java
@@ -1,7 +1,5 @@
 package com.example.lab4;
 
-import android.content.Intent;
-import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
--- lab4/app/src/main/java/com/example/lab4/MainActivity.java
+++ lab4/app/src/main/java/com/example/lab4/MainActivity.java
@@ -1,15 +1,18 @@
 package com.example.lab4;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.BroadcastReceiver;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.telephony.TelephonyManager;
 import android.widget.ArrayAdapter;
-import android.widget.BaseAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -30,16 +33,48 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         instance = this;
 
-        // register the broadcast
+
+        String requiredPermissions[] = new String[2];
+        requiredPermissions[0] = Manifest.permission.READ_PHONE_STATE;
+        requiredPermissions[1] = Manifest.permission.READ_CALL_LOG;
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CALL_LOG) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this,requiredPermissions,  1);
+        }
+        else{
+            registerReceiver();
+        }
+
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    registerReceiver();
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request.
+        }
+    }
+
+
+    private void registerReceiver(){
         IntentFilter filter = new IntentFilter(TelephonyManager.ACTION_PHONE_STATE_CHANGED);
         this.registerReceiver(myReceiver, filter);
 
-      //  adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, phoneNumberList);
+        //  adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, phoneNumberList);
         numbers_listview = (ListView)findViewById(R.id.numbers_listview);
-      //  numbers_listview.setAdapter(adapter);
+        //  numbers_listview.setAdapter(adapter);
 
         numbers_listview.setAdapter(new CustomAdapter(phoneNumberList));
-
     }
 
     @Override
