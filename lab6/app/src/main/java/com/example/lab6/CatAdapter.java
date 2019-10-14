275791a28f45d6b95a82d3f16dabf48010093fb0
ibrahimahdy <i.mahdy93@gmail.com>
2019-10-14 18:04:33 +0300

    start hw

---
+++ lab6/app/src/main/java/com/example/lab6/CatAdapter.java
@@ -0,0 +1,54 @@
+package com.example.lab6;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CatAdapter extends BaseAdapter {
+
+    List<Bitmap> imageList;
+
+    public CatAdapter(List<Bitmap> images) {
+        imageList = images;
+    }
+
+    public Bitmap getItem(int pos) {
+        return imageList.get(pos);
+    }
+
+
+    public int getCount() {
+        return imageList.size();
+    }
+
+
+    public long getItemId(int position) {
+        return imageList.get(position).hashCode();
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        ImageView imageView = new ImageView(parent.getContext());
+        if (convertView == null) {
+            imageView.setImageBitmap(getItem(position));
+            imageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
+            imageView.setLayoutParams(new AbsListView.LayoutParams(400,400));
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            imageView.setPadding(5, 5, 5 ,5);
+
+        } else{
+            imageView = (ImageView)convertView;
+        }
+        return (imageView);
+    }
+}
+
+
--- lab6/app/src/main/java/com/example/lab6/MainActivity.java
+++ lab6/app/src/main/java/com/example/lab6/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.lab6;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    EditText count;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        count = (EditText)findViewById(R.id.images_count);
+
+
+        Intent intent = new Intent(this, DownloadService.class);
+        intent.putExtra("imgCount", Integer.parseInt(count.getText().toString()));
+        startService(intent);
     }
 }
