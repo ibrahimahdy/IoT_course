83a00e5ee5a8092874186b4004a0a24e0942c55a
ibrahimahdy <i.mahdy93@gmail.com>
2019-10-15 01:58:13 +0300

    download images

--- lab6/app/src/main/java/com/example/lab6/DownloadService.java
+++ lab6/app/src/main/java/com/example/lab6/DownloadService.java
@@ -2,13 +2,24 @@ package com.example.lab6;
 
 import android.app.Service;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
 import android.os.Handler;
 import android.os.HandlerThread;
 import android.os.IBinder;
 import android.os.Looper;
 import android.os.Message;
+import android.util.Log;
 import android.widget.Toast;
 import android.os.Process;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DownloadService extends Service {
 
@@ -26,20 +37,49 @@ public class DownloadService extends Service {
         public void handleMessage(Message msg) {
             // Normally we would do some work here, like download a file.
             // For our sample, we just sleep for 5 seconds.
-            try {
-                Thread.sleep(5000);
-            } catch (InterruptedException e) {
-                // Restore interrupt status.
-                Thread.currentThread().interrupt();
+            Intent result;
+
+            for (int i=0; i<msg.arg2; i++){
+
+                result =  new Intent();
+                byte[] img = getBitmapFromURL("https://cataas.com/cat?width=400&height=400");
+                if(img.length != 0){
+                    Log.i("service", "downloaded: " + img.length);
+                    result.putExtra("result", img);
+                    result.setAction("downloadedFiles");
+                    sendBroadcast(result);
+                }
+                else{
+                    Log.i("service", "NUll download");
+                }
+
+
             }
+
             // Stop the service using the startId, so that we don't stop
             // the service in the middle of handling another job
             stopSelf(msg.arg1);
         }
     }
 
+//    @Override
+//    public void onCreate() {
+//        // Start up the thread running the service. Note that we create a
+//        // separate thread because the service normally runs in the process's
+//        // main thread, which we don't want to block. We also make it
+//        // background priority so CPU-intensive work doesn't disrupt our UI.
+//        HandlerThread thread = new HandlerThread("ServiceStartArguments", Process.THREAD_PRIORITY_BACKGROUND);
+//        thread.start();
+//
+//        // Get the HandlerThread's Looper and use it for our Handler
+//        serviceLooper = thread.getLooper();
+//        serviceHandler = new ServiceHandler(serviceLooper);
+//    }
+
     @Override
-    public void onCreate() {
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+        Log.i("service", "service started");
         // Start up the thread running the service. Note that we create a
         // separate thread because the service normally runs in the process's
         // main thread, which we don't want to block. We also make it
@@ -50,18 +90,12 @@ public class DownloadService extends Service {
         // Get the HandlerThread's Looper and use it for our Handler
         serviceLooper = thread.getLooper();
         serviceHandler = new ServiceHandler(serviceLooper);
-    }
-
-    @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        Toast.makeText(this, "service starting", Toast.LENGTH_SHORT).show();
 
         // For each start request, send a message to start a job and deliver the
         // start ID so we know which request we're stopping when we finish the job
         Message msg = serviceHandler.obtainMessage();
         msg.arg1 = startId;
         msg.arg2 = intent.getIntExtra("imgCount", -1);
-
 
         serviceHandler.sendMessage(msg);
 
@@ -80,6 +114,44 @@ public class DownloadService extends Service {
         Toast.makeText(this, "service done", Toast.LENGTH_SHORT).show();
     }
 
+
+    public byte[] getBitmapFromURL(String src) {
+        try {
+            java.net.URL url = new java.net.URL(src);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setDoInput(true);
+            connection.connect();
+            InputStream input = connection.getInputStream();
+            Bitmap myBitmap = BitmapFactory.decodeStream(input);
+
+            myBitmap = getResizedBitmap(myBitmap, 100,100);
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            myBitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+
+            return stream.toByteArray();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+
+    public Bitmap getResizedBitmap(Bitmap bm, int newHeight, int newWidth) {
+        int width = bm.getWidth();
+        int height = bm.getHeight();
+        float scaleWidth = ((float) newWidth) / width;
+        float scaleHeight = ((float) newHeight) / height;
+        // CREATE A MATRIX FOR THE MANIPULATION
+        Matrix matrix = new Matrix();
+        // RESIZE THE BIT MAP
+        matrix.postScale(scaleWidth, scaleHeight);
+
+        // "RECREATE" THE NEW BITMAP
+        Bitmap resizedBitmap = Bitmap.createBitmap(bm, 0, 0, width, height,
+                matrix, false);
+
+        return resizedBitmap;
+    }
 
 
 }
--- lab6/app/src/main/java/com/example/lab6/MainActivity.java
+++ lab6/app/src/main/java/com/example/lab6/MainActivity.java
@@ -2,14 +2,41 @@ package com.example.lab6;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.GridView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-
     EditText count;
+    GridView myView;
+    List<Bitmap> imageList = new ArrayList<>();
+    private static ArrayAdapter<Bitmap> adapter;
+
+    private final BroadcastReceiver myReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+            byte[] byteArray = intent.getByteArrayExtra("result");
+            Bitmap bmp = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.length);
+            imageList.add(bmp);
+            Log.i("numm", ""+ byteArray.length);
+        }
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +44,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        count = (EditText)findViewById(R.id.images_count);
+        IntentFilter filter = new IntentFilter();
+        filter.addAction("downloadedFiles");
+        this.registerReceiver(myReceiver, filter);
 
+
+        Button buttonOne = (Button) findViewById(R.id.button);
+        buttonOne.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                get_cats();
+            }
+        });
+    }
+
+    public void get_cats(){
+
+        Log.i("getCats", "button clicked");
+        count = (EditText)findViewById(R.id.images_count);
 
         Intent intent = new Intent(this, DownloadService.class);
         intent.putExtra("imgCount", Integer.parseInt(count.getText().toString()));
         startService(intent);
+    //    myView.setAdapter(new CatAdapter(imageList));
     }
 }
