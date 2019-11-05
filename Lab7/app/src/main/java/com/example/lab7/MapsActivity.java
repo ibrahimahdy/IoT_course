f2ae91c265ba48ccca503a98f547caf677b79d5e
ibrahimahdy <i.mahdy93@gmail.com>
2019-11-05 16:24:41 +0200

    done

--- Lab7/app/src/main/java/com/example/lab7/MapsActivity.java
+++ Lab7/app/src/main/java/com/example/lab7/MapsActivity.java
@@ -1,13 +1,13 @@
 package com.example.lab7;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
@@ -19,6 +19,18 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.maps.DirectionsApi;
+import com.google.maps.DirectionsApiRequest;
+import com.google.maps.GeoApiContext;
+import com.google.maps.model.DirectionsLeg;
+import com.google.maps.model.DirectionsResult;
+import com.google.maps.model.DirectionsRoute;
+import com.google.maps.model.DirectionsStep;
+import com.google.maps.model.EncodedPolyline;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -47,18 +59,22 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
+    LatLng destination;
+    LatLng myLocation;
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
         String requiredPermissions[] = {Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION};
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
         || ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this,requiredPermissions,  1);
         }else{
             setLoc();
+            draw(googleMap);
         }
+
     }
 
     @Override
@@ -71,23 +87,80 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
     private void setLoc(){
+
+        destination = new LatLng(58.359808, 26.677183);
+        mMap.addMarker(new MarkerOptions().position(destination).title("Lõunakeskus"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(destination));
+
         GPSLocation gps = new GPSLocation(this);
         double latitude = gps.getLatitude();
         double longitude = gps.getLongitude();
 
-        Log.i("latitude", ""+latitude);
-        Log.i("longitude", ""+longitude);
 
-        LatLng sydney = new LatLng(latitude, longitude);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Your Current Location"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        myLocation = new LatLng(latitude, longitude);
+        mMap.addMarker(new MarkerOptions().position(myLocation).title("Your Current Location"));
     }
 
 
-    public static MapsActivity getInstance() {
-        return instance;
-    }
+    private void draw(GoogleMap googleMap){
 
+        List<LatLng> path = new ArrayList<>();
+
+        GeoApiContext context = new GeoApiContext().setApiKey("AIzaSyDyno8oQ79071swJtQzGGOjIb-GcXi9GUA");
+
+        String origin = myLocation.latitude + "," + myLocation.longitude;
+        String dest = destination.latitude + "," + destination.longitude;
+        DirectionsApiRequest req = DirectionsApi.getDirections(context, origin, dest);
+
+        try {
+            DirectionsResult res = req.await();
+
+            if (res.routes != null && res.routes.length > 0) {
+                DirectionsRoute route = res.routes[0];
+
+                if (route.legs !=null) {
+                    for(int i=0; i<route.legs.length; i++) {
+                        DirectionsLeg leg = route.legs[i];
+                        if (leg.steps != null) {
+                            for (int j=0; j<leg.steps.length;j++){
+                                DirectionsStep step = leg.steps[j];
+                                if (step.steps != null && step.steps.length >0) {
+                                    for (int k=0; k<step.steps.length;k++){
+                                        DirectionsStep step1 = step.steps[k];
+                                        EncodedPolyline points1 = step1.polyline;
+                                        if (points1 != null) {
+                                            List<com.google.maps.model.LatLng> coords1 = points1.decodePath();
+                                            for (com.google.maps.model.LatLng coord1 : coords1) {
+                                                path.add(new LatLng(coord1.lat, coord1.lng));
+                                            }
+                                        }
+                                    }
+                                } else {
+                                    EncodedPolyline points = step.polyline;
+                                    if (points != null) {
+                                        List<com.google.maps.model.LatLng> coords = points.decodePath();
+                                        for (com.google.maps.model.LatLng coord : coords) {
+                                            path.add(new LatLng(coord.lat, coord.lng));
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        } catch(Exception ex) {
+            Log.e("DRAW", ex.getLocalizedMessage());
+        }
+
+        if (path.size() > 0) {
+            PolylineOptions opts = new PolylineOptions().addAll(path).color(Color.BLACK).width(6);
+            mMap.addPolyline(opts);
+        }
+        mMap.setMinZoomPreference(12);
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(myLocation));
+    }
 
 
     public void onMyLocationClick(@NonNull Location location) {
